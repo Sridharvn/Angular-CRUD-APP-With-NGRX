36718a6eb820855f52498de37c6c7748f68c94e5
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-01-09 23:54:25 +0530

    Add HttpClient support to EmployeeService for CRUD operations

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employee } from '../models/employee';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class EmployeeService {
+  api = 'http://localhost:3000/employee ';
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getEmployees() {
+    return this.http.get<Employee[]>(this.api);
+  }
+
+  getEmployeeById(id: number) {
+    return this.http.get<Employee>(`${this.api}/${id}`);
+  }
+
+  addEmployee(employee: Employee) {
+    return this.http.post<Employee>(this.api, employee);
+  }
+
+  updateEmployee(id: number, employee: Employee) {
+    return this.http.put<Employee>(`${this.api}/${id}`, employee);
+  }
+
+  deleteEmployee(id: number) {
+    return this.http.delete<void>(`${this.api}/${id}`);
+  }
 }
