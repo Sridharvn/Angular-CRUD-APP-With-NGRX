63ff179d3ebc4c8fb115427afc7e74c6b6dd0da4
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-01-11 11:42:59 +0530

    Added get employee and unsubscription

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,17 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { AddEmployeeComponent } from '../add-employee/add-employee.component';
+import { Employee } from '../../models/employee';
+import {
+  MatTable,
+  MatTableDataSource,
+  MatTableModule,
+} from '@angular/material/table';
+import { EmployeeService } from '../../services/employee.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatDialogModule],
+  imports: [MatCardModule, MatButtonModule, MatDialogModule, MatTableModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.scss',
 })
-export class EmployeeComponent {
-  constructor(private dialog: MatDialog) {}
+export class EmployeeComponent implements OnInit, OnDestroy {
+  empList: Employee[] = [];
+  dataTable!: MatTableDataSource<Employee>;
+  displayedColumn = ['id', 'name', 'doj', 'role', 'salary', 'action '];
+  subscriptions = new Subscription();
+  constructor(private dialog: MatDialog, private service: EmployeeService) {}
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+  getAllEmployees() {
+    let sub = this.service.getEmployees().subscribe((response) => {
+      this.empList = response;
+    });
+    this.subscriptions.add(sub);
+  }
+
   addEmployee() {
     this.dialog.open(AddEmployeeComponent, {
       width: '50%',
