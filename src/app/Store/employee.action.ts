16eee57714ee04509535d1ac4647811d3c4e16da
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-01-12 19:19:09 +0530

    Added employee effects

--- src/app/Store/employee.action.ts
+++ src/app/Store/employee.action.ts
@@ -4,12 +4,12 @@ import { Employee } from '../models/employee';
 export const LOAD_EMPLOYEE = 'employee getall';
 export const LOAD_EMPLOYEE_SUCCESS = 'employee getall success';
 export const LOAD_EMPLOYEE_FAILURE = 'employee getall failure';
-const loadAllEmployees = createAction(LOAD_EMPLOYEE);
-const loadAllEmployeesSuccess = createAction(
+export const loadEmployee = createAction(LOAD_EMPLOYEE);
+export const loadEmployeeSuccess = createAction(
   LOAD_EMPLOYEE_SUCCESS,
   props<{ list: Employee[] }>()
 );
-const loadAllEmployeesFailure = createAction(
+export const loadEmployeeFailure = createAction(
   LOAD_EMPLOYEE_FAILURE,
   props<{ errMsg: string }>()
 );
--- src/app/Store/employee.effects.ts
+++ src/app/Store/employee.effects.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { EmployeeService } from '../services/employee.service';
+import {
+  loadEmployee,
+  loadEmployeeFailure,
+  loadEmployeeSuccess,
+} from './employee.action';
+import { catchError, exhaustMap, map, of } from 'rxjs';
 
 @Injectable()
 export class empEffects {
   constructor(private actions$: Actions, private service: EmployeeService) {}
+
+  _loadEmployee = createEffect(() =>
+    this.actions$.pipe(
+      ofType(loadEmployee),
+      exhaustMap((action) => {
+        return this.service.getEmployees().pipe(
+          map((data) => {
+            return loadEmployeeSuccess({ list: data });
+          }),
+          catchError((err) => of(loadEmployeeFailure({ errMsg: err.message })))
+        );
+      })
+    )
+  );
 }
