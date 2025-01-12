61012f59c4ad0b0eccff0dec46df2ceb8ce4c834
Sridhar V Nampoothiripad <sridharvnampoothiripad@gmail.com>
2025-01-12 19:23:45 +0530

    Reducer completed

--- src/app/Store/employee.reducer.ts
+++ src/app/Store/employee.reducer.ts
@@ -1,7 +1,24 @@
-import { createReducer } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { employeeState } from './employee.state';
+import { loadEmployeeFailure, loadEmployeeSuccess } from './employee.action';
 
-const _employeeReducer = createReducer(employeeState);
+const _employeeReducer = createReducer(
+  employeeState,
+  on(loadEmployeeSuccess, (state, action) => {
+    return {
+      ...state,
+      list: action.list,
+      errorMessage: '',
+    };
+  }),
+  on(loadEmployeeFailure, (state, action) => {
+    return {
+      ...state,
+      list: [],
+      errorMessage: action.errMsg,
+    };
+  })
+);
 
 export function employeeReducer(state: any, action: any) {
   return employeeReducer(state, action);
